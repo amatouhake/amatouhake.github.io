4001aca81e54e4ab809bcc6a5c06d4df2c239a06
amatouhake <kenkenhagizou@gmail.com>
2022-10-20 22:29:51 +0900

    astro

--- src/components/modules/Link.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let href: string;
-</script>
-
-<span on:click={() => (window.location.href = href)}>
-  <slot />
-</span>
--- src/store.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type { SvelteComponent } from 'svelte';
-
-export const href = writable<typeof SvelteComponent>();
